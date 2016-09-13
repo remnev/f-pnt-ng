968209da8c049535c9fbafb69948a9816ef02d80
Yuriy Remnev <yura_remnev@mail.ru>
2016-09-13 11:26:16 +0300

    Unit testing setup

---
+++ app/components/header/header.directive.test.js
@@ -0,0 +1,24 @@
+import assert from 'assert';
+
+describe('myApp.header directive', () => {
+    it('worked', () => {
+        assert(true, true);
+    });
+    // var $compile;
+    // var $rootScope;
+
+    // beforeEach(module('myApp.header'));
+
+    // beforeEach(inject(function (_$compile_, _$rootScope_) {
+    //     $compile = _$compile_;
+    //     $rootScope = _$rootScope_;
+    // }));
+
+    // it('Replaces the element with the appropriate content', function () {
+    //     var element = $compile("<fp-header></fp-header>")($rootScope);
+
+    //     $rootScope.$digest();
+
+    //     expect(element.html()).toContain('123');
+    // });
+});
--- karma.conf.js
+++ karma.conf.js
@@ -1,34 +1,52 @@
-//jshint strict: false
-module.exports = function(config) {
+let webpackConfig = require('./webpack.config');
+
+webpackConfig.entry = {};
+webpackConfig.plugins = [];
+
+module.exports = config => {
   config.set({
 
     basePath: './app',
 
     files: [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'components/**/*.js',
-      'view*/**/*.js'
+      {
+        pattern: 'https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.js',
+        watched: false
+      },
+      {
+        pattern: 'https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular-route.js',
+        watched: false
+      },
+      {
+        pattern: 'bower_components/angular-mocks/angular-mocks.js',
+        watched: false
+      },
+      '../public/index.js',
+      'components/**/*.test.js'
     ],
+
+    preprocessors: {
+      'components/**/*.test.js': ['webpack']
+    },
+    webpack: webpackConfig,
+    webpackMiddleware: {
+      stats: 'errors-only'
+    },
 
     autoWatch: true,
 
-    frameworks: ['jasmine'],
+    frameworks: ['mocha'],
 
-    browsers: ['Chrome'],
+    browsers: ['Firefox'],
 
     plugins: [
-      'karma-chrome-launcher',
       'karma-firefox-launcher',
-      'karma-jasmine',
-      'karma-junit-reporter'
+      'karma-mocha',
+      'karma-mocha-reporter',
+      'karma-webpack'
     ],
 
-    junitReporter: {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
+    reporters: ['mocha']
 
   });
 };
