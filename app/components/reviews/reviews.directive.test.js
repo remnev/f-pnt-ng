0e330e69ab9609035b0496bff131f9943a4d1504
Yuriy Remnev <yura_remnev@mail.ru>
2016-11-13 22:03:12 +0300

    Fixed the tests which were broken while the CI execution

--- app/components/reviews/reviews.directive.test.js
+++ app/components/reviews/reviews.directive.test.js
@@ -32,13 +32,13 @@ describe('myApp.reviews.directive', () => {
 
     it('each review has particular nonempty elements', () => {
         Array.prototype.forEach.call(element[0].querySelectorAll('.reviews__review'), (reviewElem) => {
-            assert(reviewElem.querySelector('.reviews__review-title').innerText.length);
-            assert(reviewElem.querySelector('.reviews__review-total-vote').innerText.length);
-            assert(reviewElem.querySelector('.reviews__review-posted-at').innerText.length);
-            assert(reviewElem.querySelector('.reviews__review-author').innerText.length);
-            assert(reviewElem.querySelector('.reviews__review-car-info').innerText.length);
-            assert(reviewElem.querySelector('.reviews__review-text').innerText.length);
-            assert(reviewElem.querySelector('a.reviews__review-show-more[ng-href]').innerText.length);
+            assert(reviewElem.querySelector('.reviews__review-title').textContent.length);
+            assert(reviewElem.querySelector('.reviews__review-total-vote').textContent.length);
+            assert(reviewElem.querySelector('.reviews__review-posted-at').textContent.length);
+            assert(reviewElem.querySelector('.reviews__review-author').textContent.length);
+            assert(reviewElem.querySelector('.reviews__review-car-info').textContent.length);
+            assert(reviewElem.querySelector('.reviews__review-text').textContent.length);
+            assert(reviewElem.querySelector('a.reviews__review-show-more[ng-href]').textContent.length);
         });
     });
 });
